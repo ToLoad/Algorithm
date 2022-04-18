42789cf85eb85465f1e48c7b9b8b97111d21a528
안영원 <kublaihkan@gmail.com>
2022-04-18 16:10:11 +0900

    Solved : Baekjoon 구간 합 구하기 4

---
+++ Baekjoon/11659/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 누적합 계산
+        int[] prefix = new int[N + 1];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            prefix[i] = prefix[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        // 계산
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            sb.append(prefix[end] - prefix[start - 1] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 [Programmers : 예상 대진표](https://github.com/ToLoad/Algorithm/tree/main/Programmers/%EC%98%88%EC%83%81%20%EB%8C%80%EC%A7%84%ED%91%9C)
 
 [Programmers : 파괴되지 않은 건물](https://github.com/ToLoad/Algorithm/tree/main/Programmers/%ED%8C%8C%EA%B4%B4%EB%90%98%EC%A7%80%20%EC%95%8A%EC%9D%80%20%EA%B1%B4%EB%AC%BC)
+
+[Baekjoon : 구간 합 구하기 4](https://github.com/ToLoad/Algorithm/tree/main/Baekjoon/11659)
