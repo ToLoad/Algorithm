2680bc274f66f3e6162ce3599db0b9531a486f6f
안영원 <kublaihkan@gmail.com>
2022-07-09 12:57:56 +0900

    Failed : Baekjoon Fly me to the Alpha Centauri

---
+++ Baekjoon/1011/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int result = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            result = Integer.MAX_VALUE;
+            move(start, 0, 0, end);
+            sb.append(result + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    static void move(int cur, int speed, int cnt, int end) {
+        if (cur >= end) return;
+
+        if (cur == end - 1) {
+            if (speed >= 0 && speed <= 2) {
+                result = Math.min(result, cnt + 1);
+                return;
+            }
+            return;
+        }
+
+        if (speed - 1 >= 0) move(cur + speed - 1, speed - 1, cnt + 1, end);
+        if (speed != 0) move(cur + speed, speed, cnt + 1, end);
+        move(cur + speed + 1, speed + 1, cnt + 1, end);
+    }
+}
+
