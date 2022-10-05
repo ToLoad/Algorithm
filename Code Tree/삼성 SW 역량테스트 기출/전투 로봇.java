c5802eeea7bc427567bdefec9c7649f5f4cb6f62
Youngwon An <kublaihkan@gmail.com>
2022-10-05 09:40:25 +0900

    Refactor : Code Tree 전투 로봇
    기존 코드를 구성하고 해설을 확인하면서 각 몬스터마다 로봇과의 거리를 확인하는 것이 아닌 로봇에서 거리를 한 번만 체크하면서 몬스터를 찾게끔 설계를 변경
    
    시간 단축 612ms -> 142ms

--- Code Tree/삼성 SW 역량테스트 기출/전투 로봇.java
+++ Code Tree/삼성 SW 역량테스트 기출/전투 로봇.java
@@ -5,7 +5,7 @@ public class Main {
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 
-	static int n;
+	static int n, minDist, time;
 	static int[][] map;
 	static Robot robot;
 
@@ -15,10 +15,6 @@ public class Main {
 		map = new int[n][n];
 		robot = new Robot(0, 0, 2, 0);
 
-		// 레벨별로 몬스터 관리
-		ArrayList<Monster> monsterList[] = new ArrayList[7];
-		for (int i = 1; i < 7; i++) monsterList[i] = new ArrayList<>();
-
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
@@ -27,10 +23,6 @@ public class Main {
 					robot.r = i;
 					robot.c = j;
 					map[i][j] = 0;
-				}
-
-				if (map[i][j] != 0) {
-					monsterList[map[i][j]].add(new Monster(i, j));
 				}
 			}
 		}
@@ -43,51 +35,19 @@ public class Main {
 		 * 4. 전투 로봇은 본인의 레벨과 같은 수의 몬스터를 없앨 때마다 레벨이 상승
 		 */
 
-		int time = 0;
+		time = 0;
 		while (true) {
 			// 거리가 가장 가까운 제거 가능한 몬스터 찾기
-			boolean isCan = false;
-			Monster dMonster = new Monster(0, 0);
-			int monsterLevel = 0;
-			int minDist = Integer.MAX_VALUE;
-
-			int level = robot.level;
-			if (level > 6) level = 7; // 몬스터 최대레벨은 6이므로 거기까지만 탐색하게 최대값 설정
-			for (int i = 1; i < level; i++) {
-				for (Monster monster : monsterList[i]) {
-					int dist = getDist(monster);
-					if (dist == -1) continue; // 이동이 불가능한 곳
-					isCan = true;
-
-					// 거리가 같다면 왼쪽 상단이 먼저
-					if (dist == minDist) {
-						if (dMonster.r > monster.r) {
-							dMonster = monster;
-							monsterLevel = i;
-						}
-
-						if (dMonster.r == monster.r && dMonster.c > monster.c) {
-							dMonster = monster;
-							monsterLevel = i;
-						}
-					}
-
-					if (dist < minDist) {
-						dMonster = monster;
-						monsterLevel = i;
-						minDist = dist;
-					}
-				}
-			}
-			if (!isCan) break; // 제거할 몬스터가 없으면 종료
+			minDist = 1;
+			Monster dMonster = findNearMonster();
+			if (dMonster.r == n + 1) break; // 더 이상 진행 불가
 
 			// 몬스터 제거하고 로봇 이동
 			robot.r = dMonster.r;
 			robot.c = dMonster.c;
 			robot.kill += 1;
-			time += minDist;
-			map[dMonster.r][dMonster.c] = 0;
-			monsterList[monsterLevel].remove(dMonster);
+			map[robot.r][robot.c] = 0;
+			time += minDist; // 이동한 거리만큼 시간 증가
 
 			// 레벨업
 			if (robot.kill == robot.level) {
@@ -99,18 +59,38 @@ public class Main {
 	}
 
 
-	// 굳이 몬스터 하나씩 거리계산을 하지않고 한번의 탐색에서 같은 거리에 있는 몬스터들을 다 정리해두고
-	// 해당 몬스터 중에서 하나를 제거하는게 시간이 더 빠르고 효율적일 것 같음.
-	static int getDist (Monster monster) {
+	// BFS 탐색은 거리별로 순차적으로 탐색이 진행되므로 현재 거리에서 몬스터를 찾기 못했다면 다음 거리에서 찾고
+	// 찾았을 경우 탐색을 종료하고 찾은 몬스터를 리턴하게끔 설계
+	static Monster findNearMonster () {
+		Monster dMonster = new Monster(n + 1, n + 1);
 		boolean[][] visited = new boolean[n][n];
 		Queue<Robot> q = new LinkedList<>();
 		q.offer(new Robot(robot.r, robot.c, robot.level, 0));
 		visited[robot.r][robot.c] = true;
 
+		boolean isCan = false; // 몬스터를 찾았는지 확인 여부(못찾았다면 거리를 더 늘려야하므로)
+
 		// 여기서 kill을 거리계산용으로 사용
 		while (!q.isEmpty()) {
 			Robot cur = q.poll();
-			if (cur.r == monster.r && cur.c == monster.c) return cur.kill;
+			
+			// 최소거리에 몬스터가 없었다면 최소거리를 늘려준다.
+			if (!isCan) minDist = cur.kill;
+
+			// 현재 거리에서 처치가능한 몬스터를 발견했다면
+			if (cur.kill == minDist && map[cur.r][cur.c] < cur.level && map[cur.r][cur.c] != 0) {
+				isCan = true;
+
+				// 거리가 같다면 왼쪽 상단이 먼저
+				if (dMonster.r > cur.r) {
+					dMonster = new Monster(cur.r, cur.c);
+				}
+
+				if (dMonster.r == cur.r && dMonster.c > cur.c) {
+					dMonster = new Monster(cur.r, cur.c);
+				}
+				continue;
+			}
 
 			for (int d = 0; d < 4; d++) {
 				int nr = cur.r + dr[d];
@@ -122,7 +102,7 @@ public class Main {
 			}
 		}
 
-		return -1;
+		return dMonster;
 	}
 
 	static class Robot {
